642a7803aa5d409281c9ca1c4cb2fbc2dba813f0
Jared <jbeckersalax9@aol.com>
2022-07-23 18:06:20 -0400

    commit tree 059c8f15ba60fe3c2a769f3d2626f0dd8fa35316

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      return render json: user.items
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response(invalid)
+    render json: {errors: invalid}, status: :not_found
   end
 
 end
